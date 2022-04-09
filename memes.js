ea2daa08437d34a25be84cad70d99d4859a13af4
Santiag0122 <santiag0122@users.noreply.github.com>
2022-04-09 08:57:04 -0500

    Base para iniciar el generador de memes

---
+++ memes.js
@@ -0,0 +1,13 @@
+const boton = document.getElementsByClassName('boton');
+const arrayImagenes = ["meme1.jpg", "meme2.jpg", "meme3.jpg", "meme4.jpg", "meme5.jpg", "meme6.jpg", "meme7.jpg", "meme8.jpg", "meme9.jpg", "meme10.jpg"]
+const imagenActual = ["meme1.jpg"]
+
+for (let index = 0; index < boton.length; index++) {
+    const element = boton[index];
+    element.addEventListener("click", cambiarImagen);
+    
+}
+
+function cambiarFondo(button){
+    
+}
